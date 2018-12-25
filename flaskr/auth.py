98190c0e14b7f5a7cf6daa234f8e085be965934f
Lukun <yimng.lk@gmail.com>
2018-12-25 19:06:21 +0800

    update

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -58,7 +58,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('auth.login'))
 
         flash(error)
 
@@ -78,7 +78,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
         session.clear()
-        return redirect(url_for('index'))
+        return redirect(url_for('auth.login'))
 
 def login_required(view):
     @functools.wraps(view)
